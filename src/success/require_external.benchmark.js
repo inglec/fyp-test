8709f6f4f5f28ce2518d6fafa3fa1d15b3ccd79a
inglec <inglec@tcd.ie>
2019-03-16 13:32:29 +0000

    Add ESLint

--- src/success/require_external.benchmark.js
+++ src/success/require_external.benchmark.js
@@ -2,13 +2,13 @@ const requireExternal = require('./require_external');
 
 const partitionObject = {
   benchmark: () => {
-    const object = { 'a': 1, 'b': 2, 'c': 3 };
+    const object = { a: 1, b: 2, c: 3 };
     const isEven = number => number % 2 === 0;
 
-    return requireExternal.partitionObject(object, partition, 'even', 'odd');
-  }
+    return requireExternal.partitionObject(object, isEven, 'even', 'odd');
+  },
 };
 
 module.exports = {
   'require-external-partition-object': partitionObject,
-}
+};
